70dd47e2790381b89e6326971555d54779fe8edd
Bontinue <93024harsh@gmail.com>
2023-07-14 22:34:33 +0530

    final commit of this 3 projects

--- 2023-Mood-Calender/script.js
+++ 2023-Mood-Calender/script.js
@@ -148,15 +148,15 @@ function addDays(date, days) {
 	return result;
 }
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
+// // SOCIAL PANEL JS
+// const floating_btn = document.querySelector('.floating-btn');
+// const close_btn = document.querySelector('.close-btn');
+// const social_panel_container = document.querySelector('.social-panel-container');
 
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
+// floating_btn.addEventListener('click', () => {
+// 	social_panel_container.classList.toggle('visible')
+// });
 
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+// close_btn.addEventListener('click', () => {
+// 	social_panel_container.classList.remove('visible')
+// });
