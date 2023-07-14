4b8229dd698c56daff71808efd9ace556b10ca93
Bontinue <93024harsh@gmail.com>
2023-07-14 22:18:51 +0530

    error fixed In js

--- Random-Meel-genetor/script.js
+++ Random-Meel-genetor/script.js
@@ -58,15 +58,3 @@ const createMeal = (meal) => {
 
 
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-  social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-  social_panel_container.classList.remove('visible')
-});
