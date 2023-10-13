2dad7ffa3570fce8080d52bfcf441cb0d17796f0
Believeharsh <93024harsh@gmail.com>
2023-10-13 21:06:45 +0530

    complted

--- 3D-Animation-Box/script.js
+++ 3D-Animation-Box/script.js
@@ -7,10 +7,12 @@ btn.addEventListener('click', () => {
 });
 
 function createBoxes() {
-	for(let i=0; i<4; i++) {
-		for(let j=0; j<4; j++) {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
 			const box = document.createElement('div');
 			box.classList.add('box');
+			// console.log(j)
+			console.log(i)
 			box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
 			boxesContainer.appendChild(box);
 		}
