b2127dc4a38d796e015f045ab4e2fe229f7e61bb
Believeharsh <93024harsh@gmail.com>
2023-10-29 13:36:46 +0530

    fomated

--- Theme-Switcher/src/Context/ThemeFile.jsx
+++ Theme-Switcher/src/Context/ThemeFile.jsx
@@ -1,3 +1,7 @@
+// Context Api : Context api is used to pass global variables anywhere in the code. It helps
+// when there is a need for sharing state between a lot of nested components. It is light in weight and easier to use, to create a context just need to call React.createContext().
+// No need to install other dependencies or third party libraries like redux for state management.
+
 import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
