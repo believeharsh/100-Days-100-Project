8ab8e7e9cec4ecf383a3a14a4bff970205c3504f
Believeharsh <93024harsh@gmail.com>
2023-10-30 19:33:21 +0530

    cleared unwanted code from the components

--- BookMarks/src/Components/Footer/Footer.jsx
+++ BookMarks/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
         <footer className="bg-white border-y">
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 ">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
--- BookMarks/src/Components/Header/Header.jsx
+++ BookMarks/src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ import { Link, NavLink } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="shadow sticky z-50 top-0">
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+      <header className="shadow sticky z-50 top-0 ">
+        <nav className=" border-gray-200 px-4 lg:px-6 py-2.5 bg-black" >
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link to="/" className="flex items-center">
               <img
