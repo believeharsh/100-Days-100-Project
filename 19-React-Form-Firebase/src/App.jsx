adc5791e2f48fa6ccf5908c415b29dddea3455b8
Believeharsh <bontinue@gmail.com>
2024-04-14 15:37:24 +0530

    Completed

--- 19-React-Form-Firebase/src/App.jsx
+++ 19-React-Form-Firebase/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Form from './Components/Form'
+import Navbar from './Components/Navbar'
+import ProjectNumber from './Components/ProjectNumber'
 
 const App = () => {
   return (
     <div>
+    <Navbar/>
+    <ProjectNumber/>
     <Form/>
       
     </div>
---
+++ 19-React-Form-Firebase/src/Components/ProjectNumber.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectNumber = () => {
+  return (
+    <>
+        <div className="fixed left-[-8px] top-[55px] ">
+            <div className="bg-slate-900 border-white border-[1px] rounded-xl w-[50px] h-[50px] flex justify-center items-center">
+                <p className='text-2xl text-white'>16</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ProjectNumber
